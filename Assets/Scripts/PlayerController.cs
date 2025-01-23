a25a49a8b7a962a1a299b830f90ffdc55ffcbfb8
Josueh0207 <josueh0207@gmail.com>
2025-01-23 04:13:47 -0800

    added input system and animations(again)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,50 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
 
     public float moveSpeed;
+    public float boostSpeed;
+
     public Vector2 moveDirection;
+    private float boosting;
 
     public InputActionReference move;
+    public InputActionReference boost;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>();
+        boosting = boost.action.ReadValue<float>();
 
-        rb.velocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
+        if(boosting != 0)
+        {
+            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed * boostSpeed, moveDirection.x * moveSpeed * boostSpeed);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed, moveDirection.x * moveSpeed);
+        }
+
+        if(moveDirection != Vector2.zero)
+        {
+            animator.SetInteger("State", 1);
+
+            if(boosting != 0)
+            {
+                animator.SetInteger("State", 2);
+            }
+        }
+        else
+        {
+            animator.SetInteger("State", 0);
+
+        }
     }
 }
