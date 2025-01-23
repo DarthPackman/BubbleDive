14d9cc80a30c0db0f35beb9d75c0ab991e920e43
Josueh0207 <josueh0207@gmail.com>
2025-01-23 03:15:36 -0800

    new input system and animation added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
 
     public float moveSpeed;
+    public float boostSpeed;
+    private float boosting;
     public Vector2 moveDirection;
 
     public InputActionReference move;
+    public InputActionReference boost;
+
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>();
+        boosting = boost.action.ReadValue<float>();
 
-        rb.velocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
+
+        //CHECKING IF THE PLAYER IS BOOSTING HIMSELF.
+        if (boosting != 0) {
+            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed * boostSpeed, moveDirection.x * moveSpeed * boostSpeed);
+        } else
+        {
+            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed, moveDirection.x * moveSpeed);
+        }
+
+        //CHANGE DIRECTION OF THE PLAYER
+        if (moveDirection != Vector2.zero) { 
+            transform.forward = moveDirection;
+        }
+
+
+        //ANIMATION CODE
+        if (moveDirection.x != 0 || moveDirection.y != 0) {
+            animator.SetInteger("State", 1);
+        }else
+        {
+            animator.SetInteger("State", 0);
+        }
     }
+
 }
