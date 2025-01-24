c33152402a36b452fe444afe4cda6895a5651fc3
Josueh0207 <josueh0207@gmail.com>
2025-01-23 17:26:42 -0800

    improved input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,40 +9,54 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float boostSpeed;
 
-    public Vector2 moveDirection;
+    private Vector2 moveDirection;
+    private Vector3 movementDirection;
     private float boosting;
+    private float tempBoost;
+    public float rotationSpeed;
+    public float resetRotationSpeed;
 
     public InputActionReference move;
     public InputActionReference boost;
 
-    Animator animator;
+    public Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         moveDirection = move.action.ReadValue<Vector2>();
         boosting = boost.action.ReadValue<float>();
 
-        if(boosting != 0)
+        if (boosting != 0)
         {
-            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed * boostSpeed, moveDirection.x * moveSpeed * boostSpeed);
+            tempBoost = boostSpeed;
         }
         else
         {
-            rb.velocity = new Vector3(0, moveDirection.y * moveSpeed, moveDirection.x * moveSpeed);
+            tempBoost = 1f;
         }
 
-        if(moveDirection != Vector2.zero)
+        movementDirection = new Vector3(0, moveDirection.y, moveDirection.x);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * moveSpeed * tempBoost * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (transform.rotation != Quaternion.Euler(0, 0, 0))
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), resetRotationSpeed * Time.deltaTime);
+        }
+
+
+        if (moveDirection != Vector2.zero)
         {
             animator.SetInteger("State", 1);
 
-            if(boosting != 0)
+            if (boosting != 0)
             {
                 animator.SetInteger("State", 2);
             }
@@ -50,7 +64,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetInteger("State", 0);
-
         }
     }
 }
